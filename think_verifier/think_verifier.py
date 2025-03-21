2b18f5ac7c4f78b75337302271bfa82f5a00e080
Lucy Wingard <lucywingard97@gmail.com>
2025-03-21 16:09:17 -0700

    update think verifier

--- think_verifier/think_verifier.py
+++ think_verifier/think_verifier.py
@@ -5,7 +5,13 @@ def think_verifier(completions, **kwargs):
         has_closing = '</think>' in completion
         
         if has_opening and has_closing:
-            return 1.0
+            # Check tag order
+            opening_pos = completion.find('<think>')
+            closing_pos = completion.find('</think>')
+            if opening_pos < closing_pos:
+                return 1.0
+            else:
+                return 0
         elif has_opening or has_closing:
             return 0.5
         else:
